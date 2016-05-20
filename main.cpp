b14c143abfcb3051e1711e1f7ecf468d6e948ac7
Lev Pechersky <levpechersky@gmail.com>
2016-05-20 11:15:39 +0300

    just checking how it works

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 /*
  * main.cpp
+
  *
  *  Created on: May 20, 2016
  *      Author: E203PL
  */
-
+#include <iostream>
+using namespace std;
 
 int main(){
-
+	cout << "hello world" << endl;
 	return 0;
 }
 
