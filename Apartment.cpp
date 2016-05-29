e95fce0d3ed8e8589dd78a1300bd8040ceeda0a4
Lev Pechersky <levpechersky@gmail.com>
2016-05-29 13:45:46 +0300

    Added apartment.cpp

---
+++ Apartment.cpp
@@ -0,0 +1,9 @@
+/*
+ * Apartment.cpp
+ *
+ *  Created on: May 29, 2016
+ */
+
+
+
+
--- Apartment.h
+++ Apartment.h
@@ -3,21 +3,43 @@
 
 #include <exception>
 
-class Apartment
-{
+class Apartment {
 public:
-    class ApartmentException : public std::exception {};
-    class IllegalArgException : public ApartmentException {};
-    class OutOfApartmentBoundsException : public ApartmentException {};
+	class ApartmentException: public std::exception {};
+	class IllegalArgException: public ApartmentException {};
+	class OutOfApartmentBoundsException: public ApartmentException {};
 
-    enum SquareType {EMPTY, WALL, NUM_SQUARE_TYPES};
+	enum SquareType {EMPTY, WALL, NUM_SQUARE_TYPES};
 
-    Apartment (SquareType** squares, int length, int width, int price);
+	/* C'tors, D'tor */
+	Apartment(SquareType** squares, int length, int width, int price);
+	Apartment(const Apartment& apartment);
+	~Apartment();
+
+	/* Operators */
+	Apartment& operator= (const Apartment& apartment); //TODO should handle self-assignments
+	Apartment& operator+= (const Apartment& apartment);
+	// following 2 throw OutOfApartmentBoundsException
+	const SquareType& operator() (int row, int col) const; //TODO not 100% sure about those const's
+	SquareType& operator() (int row, int col);
+	bool operator< (const Apartment& apartment) const; //TODO maybe should be friend
+
+	/* Other methods */
+	int getTotalArea() const;
+	int getPrice() const;
+	int getLength() const;
+	int getWidth() const;
+
 private:
+	SquareType** layout;
+	int length, width, price;
 };
 
 /*****************************
  * External operator function
  ****************************/
+
+//TODO this one should be implemented using += operator
+Apartment operator+(const Apartment& apartment_1, const Apartment& apartment_2);
 
 #endif //MTM4_APARTMENT_H
