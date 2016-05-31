654177bd46bd3671e79e9fc8eab15a31f875a849
batelcarmona <batelcarmona@campus.technion.ac.il>
2016-05-31 22:25:59 +0300

    test commit

--- main.cpp
+++ main.cpp
@@ -1,15 +1,8 @@
-/*
- * main.cpp
-
- *
- *  Created on: May 20, 2016
- *      Author: E203PL
- */
 #include <iostream>
 using namespace std;
 
-int main(){
-	cout << "Hello world!" << endl;
-	return 0;
-}
+//int main(){
+//	cout << "Hello world!" << endl;
+//	return 0;
+//}
 
