aa39a13feb0cf42c6e50fbad9993f592fe1ee61e
Lev Pechersky <levpechersky@gmail.com>
2016-06-04 15:27:42 +0300

    New Working Version

--- SortedSet.h
+++ SortedSet.h
@@ -3,25 +3,30 @@
 #include <assert.h>
 #include <functional>
 
+template<class T, class Compare = std::less<T> >
+class SortedSet;
+
 // ****************************************************************************
 // ***************************** node class ***********************************
 // ****************************************************************************
 
-template <class T>
+template<class T, class Compare = std::less<T> >
 class Node {
-private:
+public:
 	T* data;
 	Node* next;
 
-	template <T, class Compare = std::less<T> >
-	friend class SortedSet<T,Compare>;
+	friend class SortedSet<T, Compare> ;
 	//constructor
 	Node(const T &element) : data(new T(element)), next(nullptr) {}
+	//constructor
+	Node() : data(NULL), next(NULL) {}
 	//copy constructor
 	Node(const Node& node) : data(new T(node.data)), next(node.next) {}
 	//destructor
 	~Node() {
 		delete data;
+		delete this;
 	}
 	//assignment operator (=):
 	Node& operator=(const Node& node) {
@@ -36,7 +41,8 @@ private:
 // ****************************************************************************
 // ***************************** SortedSet class ******************************
 // ****************************************************************************
-template <class T, class Compare = std::less<T> >
+
+template<class T, class Compare>
 class SortedSet {
 public:
     class Iterator {
@@ -55,12 +61,12 @@ public:
 	     * return:
 	     * reference to this.
     	 */
-    	Iterator& operator=(const Iterator& iterator) {
-    		node = iterator.node;
-    		return *this;
-    	}
+		Iterator& operator=(const Iterator& iterator) {
+			node = iterator.node;
+			return *this;
+		}
 
-    	/* Iterator's destructor:
+		/* Iterator's destructor:
     	 * The function deletes the iterator.
     	 */
     	~Iterator() {
@@ -69,7 +75,7 @@ public:
 
        /**************Operators of iterator******************************/
 
-    	/* Iterator promoting
+		/* Iterator promoting
     	 * There are two versions:
     	 * 1. pre version (++iterator)
     	 * 2. post version (iterator++)
@@ -115,49 +121,93 @@ public:
 		bool operator!=(const Iterator& iterator){
 			return !(*this==iterator);
 		}
-    private:
-    	Node<T>* node;
-    	friend class SortedSet<T>;
 
-    	/* Iterator's constructor:
-      	 * The function creates the iterator.
-    	 * @param node: a node pointer which will be the first element.
-    	 * if the node pointer is not provided than the set will be empty.
-    	 * return:
-    	 * a new iterator object.
-    	 */
-    	Iterator(Node<T>* node = nullptr) : node(node) {}
-    };
+		/* Iterator constructor:
+		 * @param node: a pointer to node, new iterator will point to.
+		 * 		if <node> is null, new iterator is considered pointing to end.
+		 * return: new Iterator.
+		 */
+		Iterator(Node<T>* node = nullptr) :	node(node) {}
+
+	private:
+		friend class SortedSet<T, Compare> ;
+		Node<T>* node;
+	};
+	
 	/* Sets Iterator to the first element
 	 * return:
-	 *
+	 * 
 	 */
-     Iterator begin() const;
-
+	Iterator begin() {
+		iterator.node = first; //empty node first...
+		return iterator;
+	}
+	
 	/* Sets Iterator to the end of set.
 	 * As a result, iterator doesn't point to valid element,
 	 * e.g. you can't dereference iterator after end().
 	 * return:
 	 *
 	 */
-    Iterator end() const;
-
+	Iterator end() {
+		for (begin(); iterator.node->data; iterator++);
+		return iterator;
+	}
+	
 	/* Searches the set for an item, that equals <element>.
 	 * If found - iterator will point to found item.
 	 * Otherwise, iterator will point to end().
 	 * return:
 	 *
 	 */
-    const Iterator find(const T& element) const;
-
+	Iterator find(const T& element) {
+		Iterator end = Iterator();
+		begin();
+		while (iterator != end && Compare()(*iterator, element)) {
+			++iterator;
+		}
+		//now i is null or larger or equal
+		if (iterator == end || Compare()(element, *iterator)) { //if i is null or larger than <element>
+			return end;
+		}
+		return iterator;
+	}
+	
 	/* Adds an element to set, if it wasn't contained in the set before.
+	 * Items are added in a way, that set stays ordered (as defined by
+	 * Compare template parameter)
 	 *
 	 * return:
 	 * true - if set did not contain <element> before (and it was added)
 	 * false - if set did contain <element> before. Set stays unchanged
 	 * 	in that case.
 	 */
-    bool insert(const T& element);
+	bool insert(const T& element) {
+		if (!first) { //Set empty - insert anyway
+			first = new Node<T>(element);
+			first->next = nullptr;
+			return true;
+		}
+		if (Compare()(element, *(first->data))) { //All items in set are greater than <element> - insert first
+			Node<T>* tmp = new Node<T>(element);
+			tmp->next = first;
+			first = tmp;
+			return true;
+		}
+		Iterator previous = begin(), end = Iterator();
+		iterator++;
+		while (iterator != end && Compare()(*iterator, element)) {
+			++previous;
+			++iterator;
+		}
+		if (iterator == end || Compare()(element, *iterator)) { //if next is empty or greater than <element> - insert after previous
+			Node<T>* tmp = new Node<T>(element);
+			tmp->next = previous.node->next;
+			previous.node->next = tmp;
+			return true;
+		}
+		return false;
+	}
 
 	/* Removes an element from the set.
 	 *
@@ -165,13 +215,17 @@ public:
 	 * true - if <element> found and has been removed
 	 * false - if <element> has not been found
 	 */
-    bool remove(const T& element);
+    bool remove(const T& element){
+    	return true;//TEMP
+    }
 
 	/*
 	 * return:
 	 * number of elements in set
 	 */
-    int size() const;
+    int size() const{
+    	return 0;//TEMP
+    }
 
 	/*
 	 * return:
@@ -199,149 +253,69 @@ public:
 	 */
     SortedSet operator^(const SortedSet&) const;//TODO probably should be implemented as (A|B)-(A&B)
 
-    ~SortedSet();
-    SortedSet(const SortedSet& set);
-    SortedSet& operator=(const SortedSet& set);
-    SortedSet();
+	/* SortedSet constructor.
+	 *
+	 * return:
+	 * new empty SortedSet
+	 */
+	SortedSet() : first(), iterator(first) {}
 
+	/* SortedSet destructor.
+	 */
+	~SortedSet() { removeList(first); }
+
+	/* SortedSet copy constructor.
+	 *
+	 * return:
+	 * new SortedSet, containing all members of <set>
+	 */
+	SortedSet(const SortedSet& set) : first(new Node<T>()), iterator() {
+		copyList(first, set.first);
+		if (set.iterator.node->data) {
+			begin();
+			return;
+		}
+		end();
+	}
+
+	/* SortedSet assignment operator ( = ).
+	 */
+	SortedSet& operator=(const SortedSet& set) {
+		Node<T>* helper = new Node<T>();
+		copyList(helper, set.first);
+		removeList(first);
+		first = helper;
+		return *this;
+	}
 
 private:
-    Node<T>* first;
-    Iterator iterator;
+	Node<T>* first;
+	Iterator iterator;
+
+	/* Destroys Linked list */
+	static void removeList(Node<T>* first) {
+		if (first->next == nullptr) {
+			return;
+		}
+		Node<T>* helper = first->next;
+		while (helper) {
+			Node<T>* next = helper->next;
+			delete helper;
+			helper = next;
+		}
+		delete first;
+	}
+
+	/* Copies Linked List */
+	static void copyList(Node<T>* to_copy, Node<T>* copy) {
+		Node<T>* helper1 = to_copy->next;
+		Node<T>* helper2 = copy->next;
+		while (helper1) {
+			helper2 = new Node<T>(helper1->data);
+			helper2 = helper2->next;
+			helper1->next;
+		}
+	}
 };
-
-template<class T, class Compare>
-typename SortedSet<T, Compare>::Iterator SortedSet<T, Compare>::begin() const {
-	return Iterator(first);
-}
-
-template<class T, class Compare>
-typename SortedSet<T, Compare>::Iterator SortedSet<T, Compare>::end() const {
-	return Iterator();
-}
-
-template<class T, class Compare>
-const typename SortedSet<T, Compare>::Iterator SortedSet<T, Compare>::find(
-		const T& element) const {
-	auto i = begin(), last = end();
-	while (i != last && Compare()(*i, element)) {
-		++i;
-	}
-	//now i is null or larger or equal
-	if (i == last || Compare()(element, *i)) {//if i is null or larger than <element>
-		return last;
-	}
-	return i;
-}
-
-template<class T, class Compare>
-bool SortedSet<T, Compare>::insert(const T& element) {
-	if (!first) {
-		first = new Node<T>(element);
-		first->next = nullptr;
-		return true;
-	}
-	Iterator i = begin(), end = this->end(), next=++i;
-	while (next != end && Compare()(*(next), element)) {
-		++i;
-		++next;
-	}
-	//i points to last valid item, that is lower or equal to <element>
-	if (Compare()(*i, element)) {//if i lower than <element>, insert after i
-		Node<T>* tmp = new Node<T>(element);
-		tmp->next = i.node->next;
-		i.node->next = tmp;
-		return true;
-	}
-	return false;
-}
-
-template<class T, class Compare>
-bool SortedSet<T, Compare>::remove(const T& element) {
-	return true; //TEMP
-}
-
-template<class T, class Compare>
-int SortedSet<T, Compare>::size() const {
-     int count=0;
-	 for(begin();iterator.node->next;iterator++){
-	 count++;
-  }
-  return count;
-}
-
-template<class T, class Compare>
-SortedSet<T, Compare> SortedSet<T, Compare>::operator&(const SortedSet&) const {
-	return SortedSet();//TEMP
-}
-
-/*
- * return:
- * new SortedSet, containing an union of two sets.
- */
-template<class T, class Compare>
-SortedSet<T, Compare> operator|(const SortedSet<T, Compare>& set1, const SortedSet<T, Compare>& set2 ){
-	int size1=set1.size();
-	int size2=set2.size();
-	SortedSet<T, Compare>& temp_set;
-	//for(int i=0; i<size1 && i<size2; i++)TODO
-}
-
-template<class T, class Compare>
-SortedSet<T, Compare> SortedSet<T, Compare>::operator-(const SortedSet&) const {
-	return SortedSet();//TEMP
-}
-
-template<class T, class Compare>
-SortedSet<T, Compare> SortedSet<T, Compare>::operator^(const SortedSet&) const {
-	return SortedSet();//TEMP
-} //TODO probably should be implemented as (A|B)-(A&B), maybe as non-member
-
-
-
-template<class T, class Compare>
-SortedSet<T, Compare> ::~SortedSet() {
-	Node<T>* current = first;
-	if (first == NULL) {
-		return;
-	}
-	while (! current->next) {
-		Node<T>* next = current->next;
-		delete current;
-		current = next;
-	}
-	delete current;
-	first=NULL;
-	return;
-}
-
-
-template<class T, class Compare>
-SortedSet<T, Compare> ::SortedSet():
-    first(nullptr), iterator(){
-}
-
-template<class T, class Compare>
-SortedSet<T, Compare> ::SortedSet(const SortedSet<T, Compare>& set){
-	if (set.first == nullptr) {
-		first = NULL;
-		return;
-	}
-	first = new Node<T> (*((set.first)->data));
-	Iterator set_end = set.end();
-	if (iterator == set_end ) {
-		return;
-	}
-	while( iterator != set_end ) {
-		iterator++;
-		insert(*iterator);
-	}
-	return;
-}
-
-template<class T, class Compare>
-SortedSet<T, Compare>& SortedSet<T, Compare> ::operator=(const SortedSet& set){
-
-}
 
 #endif //MTM4_SORTEDSET_H
--- main.cpp
+++ main.cpp
@@ -4,12 +4,24 @@ using namespace std;
 
 int main(){
 	cout << "Hello world!" << endl;
-	SortedSet<int, std::less<int>> set;
-	set.find(5);
+	SortedSet<int> set;
 	set.insert(5);
 	set.insert(3);
 	set.insert(8);
-	cout << *(set.find(5)) << endl;
+	set.insert(1);
+	set.insert(7);
+	set.insert(6);
+	set.insert(11);
+	set.insert(25);
+	set.insert(8);
+	set.insert(4);
+	//set: 1 3 4 5 6 7 8 11 25
+	SortedSet<int>::Iterator i;
+	i = set.find(8);
+	i = set.find(1);
+	i = set.find(25);
+	i= set.find(10000);
+	i = set.find(-1);
 
 	return 0;
 }
